29c5cf8c71a13a283af945ce14718ffbf8ca553b
Funto <funto.afuwape@gmail.com>
2023-04-04 14:49:58 +0100

    Create "Game" class
    Created a game class that initializes an array for the game and two players using the functionality in the "Player" class.
    
    A print_board method was created within the Game class.

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,3 +1,7 @@
+# module WinningLines
+#   winning_lines = Array.new([1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7])
+# end
+
 class Player
   @@num_of_players = 0
   @@player_symbols = ["X", "O"]
@@ -6,9 +10,14 @@ class Player
   attr_accessor :name
 
   def initialize(name)
-    @name = name
-    @symbol = player_symbol
-    @@num_of_players += 1
+    if @@num_of_players <= 2
+      @name = name
+      @symbol = player_symbol
+      @@num_of_players += 1  
+    else
+      return puts "Maximum players attained. Something isn't right here..."
+    end
+    
   end
 
   protected 
@@ -26,8 +35,24 @@ class Player
   end
 end
 
-player1 = Player.new("Player 1")
-puts player1
+class Game
+  def initialize
+    @board = Array.new(9)
+    @player1 = Player.new("Player 1")
+    @player2 = Player.new("Player 2")
+  end
+  
+  def print_board
+    @board.each_index do |idx|
+      if idx == 3 || idx == 6
+        print "\n"
+      end
+      print "|__|"
+    end
+    print "\n"
+  end
 
-player2 = Player.new("Player 2")
-puts player2
+end
+
+game = Game.new
+game.print_board
