ceaf3111b8507cba26fb88b426a4487963913204
Funto <funto.afuwape@gmail.com>
2023-04-11 15:36:56 +0100

    Create a player_move method
    A method to allow players chose where they want to play is being created in the "Game" class

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,6 +1,6 @@
-# module WinningLines
-#   winning_lines = Array.new([1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7])
-# end
+module WinningLines
+  winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+end
 
 class Player
   @@num_of_players = 0
@@ -32,11 +32,12 @@ class Player
 end
 
 class Game
-  attr_accessor :players_hash
+  attr_reader 
+  attr_accessor :players_hash, :free_spaces
 
   def initialize
     @board = Array.new(9)
-    @free_space = (0..8).to_a
+    @free_spaces = (1..9).to_a
     @player1 = Player.new("Player 1")
     @player2 = Player.new("Player 2")
     @players_hash = [@player1.player_array, @player2.player_array].to_h
@@ -52,7 +53,17 @@ class Game
     print "\n"
   end
 
+  def player_move
+    puts "Where would you like to play?"
+    play_slot = gets.to_i
+
+    if play_slot > 9 || play_slot < 1
+      puts "This move is out of bounds. Please choose a valid slot."
+      self.player_move 
+    end
+  end
 end
 
 game = Game.new
 p game.players_hash
+game.player_move
